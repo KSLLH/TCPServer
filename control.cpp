bd79296dda5ccdf6ce8c778cb450701e777888b2
DengKunxiao <keshenglilihui@sina.com>
2020-10-18 16:27:48 +0800

    i

---
+++ control.cpp
@@ -0,0 +1,20 @@
+#include <glog/logging.h>
+
+#include "control.h"
+#include "service_manager.h"
+
+static Control& Control::GetInstance(){
+	static Control instance;
+	return instance;
+}
+
+void Control::Start(){
+	DLOG(INFO) << "Start ServiceManager";
+	ServiceManager::GetInstance.Start();
+}
+
+void Control::Stop(){
+	//pass
+}
+
+Control::Control(){}
