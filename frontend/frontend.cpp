d3a5b8fde4316c71fe54c1742044d2819dcb4745
DengKunxiao <keshenglilihui@sina.com>
2020-10-19 00:09:11 +0800

    i

--- frontend/frontend.cpp
+++ frontend/frontend.cpp
@@ -37,11 +37,11 @@ int main(int argc, char* argv[]){
 		Daemonize();
 	}
 	
-	//ThreadPool thread_pool {FLAGS_thread_size};
-	//auto res = thread_pool.enqueue(Task, FLAGS_address, FLAGS_port, "test text");
-
-	
+	ThreadPool thread_pool {FLAGS_thread_size};
 	std::cout << Task(FLAGS_address, FLAGS_port, "test text") << std::endl;
+	//for(;;){
+	//	thread_pool.enqueue(Task, FLAGS_address, FLAGS_port, "test text")
+	//}
 }
 
 inline void Daemonize(){
@@ -62,6 +62,11 @@ inline void Daemonize(){
 }
 
 int Task(const std::string& address, const int& port, const std::string& str){
+	int sockfd = TCPConnection(address, port);
+	return CallCalcService(sockfd, str);
+}
+
+int TCPConnection(const std::string & address, const int& port){
 	int sockfd;
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		std::cout << "socket error: " << strerror(errno) << std::endl;
@@ -71,6 +76,7 @@ int Task(const std::string& address, const int& port, const std::string& str){
 	struct sockaddr_in sock_addr;
 	bzero(&sock_addr, sizeof(sock_addr));
 	sock_addr.sin_family = AF_INET;
+	sock_addr.sin_port = htons(port);
 	const char* ip = address.c_str();
 	if(inet_pton(AF_INET, ip, &sock_addr.sin_addr) <= 0){
 		std::cout << "inet_pton error: " << strerror(errno);
@@ -80,9 +86,9 @@ int Task(const std::string& address, const int& port, const std::string& str){
 	if(connect(sockfd, (struct sockaddr*)&sock_addr, sizeof(sock_addr)) < 0){
 		std::cout << "connect error: " << strerror(errno);
 		abort();
-	}	
-	
-	return CallCalcService(sockfd, str);
+	}
+
+	return sockfd;
 }
 
 int CallCalcService(const int& sockfd, const std::string& str){
@@ -91,6 +97,7 @@ int CallCalcService(const int& sockfd, const std::string& str){
 		char str[sizeof(int)];
 	} ans;
 	char buf[20];
+	bzero(buf, sizeof(buf));
 	int nbyte;
 	for(nbyte = 0; nbyte == 19 || nbyte == str.size() - 1; nbyte++){
 		buf[nbyte] = str[nbyte];
